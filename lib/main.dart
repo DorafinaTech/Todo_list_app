75aa3d26bde76e6baf67200acf7e126643d31659
dorafina <thessyzilla@gmail.com>
2024-03-05 15:30:21 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_list_app/auth/authscreen.dart';
+import 'package:todo_list_app/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return Home();
+          } else {
+            return AuthScreen();
+          }
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(brightness: Brightness.dark, primaryColor: Colors.purple),
+    );
+  }
+}
